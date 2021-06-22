0b039f38881113db4cd1975913c3eb82d92792a4
amir <roshandel2004@gmail.com>
2021-06-22 21:45:37 +0430

    login page

--- www/sadaf/login.php
+++ www/sadaf/login.php
@@ -3,95 +3,156 @@ session_start();
 ?>
 
 <!doctype html>
+<head>
+    <title>Login SOSO</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="loginStyle/images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/css/util.css">
+    <link rel="stylesheet" type="text/css" href="loginStyle/css/main.css">
+    <!--===============================================================================================-->
+</head>
 <!-- Programmer: Omid MilaniFard -->
 <?php
-	include "sys_config.class.php";
-	require_once "DateUtils.inc.php";
-	require_once "SharedClass.class.php";
-	require_once "UI.inc.php";
-	
-	HTMLBegin();
-	$username = $_SESSION["UserID"];
-    $_SESSION["UserID"] = null;
+include "sys_config.class.php";
+require_once "DateUtils.inc.php";
+require_once "SharedClass.class.php";
+require_once "UI.inc.php";
 
-	$message = "";
-	if(isset($_REQUEST["UserID"]))
-	{
-		// در این نسخه از چارچوب نرم افزاری کلمه عبور به صورت متن خام ذخیره شده است
-		// برای نسخه های عملیاتی حتما از رمزنگاری مناسب استفاده شود - مثال: md5
-		// می توان از ldap هم استفاده کرد
-		$mysql = pdodb::getInstance();
-		$mysql->Prepare("select * from sadaf.AccountSpecs 
-											JOIN sadaf.persons using (PersonID) 
-											where UserID=? and UserPassword=?");
-		$res = $mysql->ExecuteStatement(array($_REQUEST["UserID"], md5($_REQUEST["UserPassword"])));
-		
-		if($trec = $res->fetch())
-		{
-			session_start();
-			$_SESSION["UserID"] = $trec["UserID"];
-			$_SESSION["SystemCode"] = 0;
-			$_SESSION["PersonID"] = $trec["PersonID"];
-			$_SESSION["UserName"] = $trec["pfname"]." ".$trec["plname"];
-			$_SESSION["LIPAddress"] = ip2long(SharedClass::getRealIpAddr());
-			if($_SESSION["LIPAddress"]=="") {
-                $_SESSION["LIPAddress"] = 0;
-            }
-			echo "<script>document.location='main.php';</script>";
-			die();
-		}
-		else
-			$message = "نام کاربر یا کلمه عبور نادرست است";
-	}
+
+
+$_SESSION["UserID"] = null;
+
+$message = "";
+if(isset($_REQUEST["UserID"]))
+{
+    // در این نسخه از چارچوب نرم افزاری کلمه عبور به صورت متن خام ذخیره شده است
+    // برای نسخه های عملیاتی حتما از رمزنگاری مناسب استفاده شود - مثال: md5
+    // می توان از ldap هم استفاده کرد
+    $mysql = pdodb::getInstance();
+    $mysql->Prepare("select * from sadaf.user where username=? and pass=?");
+
+    $res = $mysql->ExecuteStatement(array($_REQUEST["UserID"], ($_REQUEST["UserPassword"])));
+    if($trec = $res->fetch())
+    {
+        session_start();
+        $_SESSION["UserID"] = $trec["userId"];
+        $_SESSION["SystemCode"] = 0;
+        $_SESSION["username"] = $trec["username"];
+        $_SESSION["LIPAddress"] = ip2long(SharedClass::getRealIpAddr());
+        if($_SESSION["LIPAddress"]=="") {
+            $_SESSION["LIPAddress"] = 0;
+        }
+        header("Location: main.php");
+        die();
+    }
+    else
+        $message = "نام کاربر یا کلمه عبور نادرست است";
+}
 ?>
+<body>
 
-<body >
-<form method=post>
+    <div class="container-fluid">
+        <? if($message!="") { ?>
+        <div class="row">
+            <div class="col-1" ></div>
+            <div class="col-10" >
+                <div class="alert alert-danger well" role="alert"><?php echo $message; ?></div>
+            </div>
+            <div class="col-1" ></div>
+        </div>
+    </div>
+    <? } ?>
 
-<div class="container-fluid">
-<? if($message!="") { ?>
-<div class="row">
-	<div class="col-1" ></div>
-	<div class="col-10" >
-		<div class="alert alert-danger well" role="alert"><?php echo $message; ?></div>
-	</div>
-	<div class="col-1" ></div>
-	</div>
-</div>
-<? } ?>
-<div class="row">
-<div class="col-3" ></div>
-<div class="col-6" >
-	<br>
-	<div class="portlet box green">
-	<div class="portlet-title">
-		<div class="caption">
-			چارچوب توسعه نرم افزار سدف
-		</div>
-	</div>
-	<div class="portlet-body">
-			<table class="table">
-				<tr>
-					<td>نام کاربری</td>			
-				<td><input type=text name=UserID id=UserID class="form-control" value=<?php echo $_SESSION["UserID"];
-                    $_SESSION["UserID"] = null; ?>></td>
-				</tr>
-				<tr>
-					<td>کلمه رمز</td>			
-					<td><input type=password id=UserPassword name=UserPassword class="form-control"></td>
-				</tr>
-				<tr>
-					<td colspan=2 align=center>
-					<button type="submit" class="btn btn-primary active">ورود</button>
-					</td>
-				</tr>
-			</table>
-	</div>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-form-title" style="background-image: url(loginStyle/images/bg-01.jpg);">
+                        <span class="login100-form-title-1">
+                            Log In
+                        </span>
+                </div>
 
-</div>
-<div class="col-3" ></div>
-</div>
+                <form class="login100-form validate-form" method="post">
+                    <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+                        <span class="label-input100">Username</span>
+                        <input class="input100" type="text" name="UserID" id="UserID" placeholder="Enter username" value=<?php echo $_SESSION["UserID"];
+                        $_SESSION["UserID"] = null; ?>>
+                        <span class="focus-input100"></span>
+                    </div>
 
-</form>
-</div>
+                    <div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+                        <span class="label-input100">Password</span>
+                        <input class="input100" type="password" name="UserPassword" id="UserPassword" placeholder="Enter password">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <div class="flex-sb-m w-full p-b-30">
+
+                        <!-- redirect to password reset page -->
+                        <?php
+                        if(isset($_GET["newpwd"])){
+                            if($_GET["newpwd"] == "passwordupdated"){
+                                echo "<p class=''>Your password has been reset!</p>";
+                            }
+                        }
+                        ?>
+                        <div>
+                            <a href="forgottenpwd/reset-password.php" class="txt1">
+                                Forgot Password?
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex-sb-m w-full p-b-30">
+                        <div class="container-login100-form-btn">
+                            <button type="submit" class="login100-form-btn">
+                                Login
+                            </button>
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <a href="signup.php" class="login100-form-btn">
+                                Signup
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!--===============================================================================================-->
+    <script src="loginStyle/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="loginStyle/vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="loginStyle/vendor/bootstrap/js/popper.js"></script>
+    <script src="loginStyle/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="loginStyle/vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="loginStyle/vendor/daterangepicker/moment.min.js"></script>
+    <script src="loginStyle/vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="loginStyle/vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="loginStyle/js/main.js"></script>
+
 </body>
