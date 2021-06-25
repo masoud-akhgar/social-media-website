e5d402c75906326a56768b93a701cd7a010a534b
Mahdi <mahdipersia79@gmail.com>
2021-06-25 10:58:25 +0430

    like and comment in post

--- www/sadaf/post.php
+++ www/sadaf/post.php
@@ -4,7 +4,7 @@ $mysql = pdodb::getInstance();
 
 $mysql->Prepare("select * from sadaf.post where postId= ? ");
 $res = $mysql->ExecuteStatement(array($_GET['post']));
-
+$post = $res->fetch();
 
 $mysql->Prepare("select * from sadaf.comment, sadaf.profile where 
                                     sadaf.profile.userId=sadaf.comment.userId and postId=?");
@@ -27,6 +27,7 @@ $userid = $_SESSION['UserID'];
     <script src="../jquery/jquery-3.4.1.min.js.txt"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 
@@ -45,7 +46,7 @@ $userid = $_SESSION['UserID'];
             <div class="asset col-5 text-left">
                 <ul class="nav d-flex">
                     <li class="nav-item">
-                        <a href="" class="nav-link text-white "> <img class="profile-header" src="asset/images/home.png" alt=""></a>
+                        <a href="main.php" class="nav-link text-white "> <img class="profile-header" src="asset/images/home.png" alt=""></a>
                     </li>
                 </ul>
             </div>
@@ -80,7 +81,10 @@ $userid = $_SESSION['UserID'];
 </header>
 <?php include("right_side.php"); ?>
 <?
-$userProfile = $mysql->Execute("SELECT * FROM sadaf.profile WHERE userId=$userid");
+$postuserid = $post['userId'];
+//$userProfile = $mysql->Execute("SELECT * FROM sadaf.profile WHERE userId=$userid");
+//$rec = $userProfile->fetch();
+$userProfile = $mysql->Execute("SELECT * FROM sadaf.profile WHERE userId=$postuserid");
 $rec = $userProfile->fetch();
 ?>
 <div class="profile2" style="margin-top: 70px; margin-left: 350px ">
@@ -96,20 +100,32 @@ $rec = $userProfile->fetch();
                 <div class="post-title-time">
                     <a href="">@<?echo $rec["username"]?></a>
                 </div>
-                <div class="post-desc">
-                    <?echo $res->fetch()['text'];?>
+                <div class="post-desc" style="font-size: 25px; text-align: right; direction: rtl">
+                    <?echo $post['text'];?>
                     <br>
-                    <img src="asset/images/Computer.jpg" class="w-100">
+                    <? if($post["image"] != null){?>
+                        <div class="w-100" style="height: 500px;overflow: hidden;">
+                            <img src="./postImg/<?echo $post['image']?>" class="img-fluid">
+                        </div>
+                    <?} ?>
                 </div>
                 <div class="mt-3 bg-white w-100 px-2 py-2 shadow-bottom post pl-4">
                     <p class="d-inline" style="font-size: 14px;">like 3 comment 1</p>
                     <div class="w-100">
-                        <div class=" kadr w-100 d-flex post-detail">
-                            <p class="text-rights detail like">
-                                <a href=""><i class="fa fa-thumbs-up"></i> LIKE</a>
-                            </p>
-                            <p class="text-right detail">
-                                <a href=""><i class="fa fa-comments"></i> COMMENT</a>
+                        <div class=" kadr w-100 d-flex justify-content-between post-detail">
+                            <p class="text-rights detail">
+                                <?
+                                $results = $mysql->Execute( "SELECT * FROM sadaf.likes WHERE userId=".$_SESSION['UserID']." AND postId=".$_GET['post']);
+                                //
+                                $cnt =  intval(count($results->fetch()));
+                                if ($cnt>1){ ?>
+                                    <span class="unlike fa fa-heart fa-lg"  style="color: red" data-id="<?php echo $_GET['post']; ?>"></span>
+                                    <span class="like hide fa fa-heart-o fa-lg" style="color: red" data-id="<?php echo $_GET['post']; ?>"></span>
+                                <? }else{ ?>
+                                    <!-- user has not yet liked post -->
+                                    <span class="like fa fa-heart-o fa-lg" style="color: red" data-id="<?php echo $_GET['post']; ?>"></span>
+                                    <span class="unlike hide fa fa-heart fa-lg" style="color: red" data-id="<?php echo $_GET['post']; ?>"></span>
+                                <? } ?>
                             </p>
                             <p class="text-right detail">
                                 <a href=""><i class="fa fa-share"></i> SHARE</a>
@@ -151,6 +167,48 @@ $rec = $userProfile->fetch();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
     $(document).ready(function(){
+        // when the user clicks on like
+        $('.like').on('click', function(){
+            var postid = $(this).data('id');
+            $post = $(this);
+
+            $.ajax({
+                url: 'main.php',
+                type: 'post',
+                data: {
+                    'liked': 1,
+                    'postid': postid
+                },
+                success: function(response){
+                    $post.parent().find('span.likes_count').text(response + " likes");
+                    $post.addClass('hide');
+                    $post.siblings().removeClass('hide');
+                }
+            });
+        });
+
+        // when the user clicks on unlike
+        $('.unlike').on('click', function(){
+            var postid = $(this).data('id');
+            $post = $(this);
+
+            $.ajax({
+                url: 'main.php',
+                type: 'post',
+                data: {
+                    'unliked': 1,
+                    'postid': postid
+                },
+                success: function(response){
+                    $post.parent().find('span.likes_count').text(response + " likes");
+                    $post.addClass('hide');
+                    $post.siblings().removeClass('hide');
+                }
+            });
+        });
+    });
+
+    $(document).ready(function(){
         $('.iscomment').keydown(function(event){
             var keyCode = (event.keyCode ? event.keyCode : event.which);
             if (keyCode == 13) {
@@ -173,6 +231,5 @@ $rec = $userProfile->fetch();
             }
         });
     });
-
 </script>
 </html>
