c457d44657d1d5448d7010674ef565aa21619d93
amir <roshandel2004@gmail.com>
2021-06-25 12:44:33 +0430

    change bg img

--- www/sadaf/createPost.php
+++ www/sadaf/createPost.php
@@ -45,7 +45,7 @@ $username = $_SESSION["UserID"];
     <div class="limiter">
         <div class="container-login100">
             <div class="wrap-login100">
-                <div class="login100-form-title" style="background-image: url(loginStyle/images/bg-01.jpg);">
+                <div class="login100-form-title" style="background-image: url(loginStyle/images/post.png);">
                         <span class="login100-form-title-1">
                             create post
                         </span>
