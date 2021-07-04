fec1f0bd3d33806f8ff744c5c699ac8067ac8f1a
Matin Koohjani <we1257n@gmail.com>
2021-07-04 17:33:18 +0430

    db - some cleanings

--- www/sadaf/profile.php
+++ www/sadaf/profile.php
@@ -1,35 +1,8 @@
 <?php
 include('header.inc.php');
-$mysql = pdodb::getInstance();
-$userid = $_SESSION['UserID'];
-$result = $mysql->Execute("SELECT * FROM sadaf.user WHERE userId=$userid");
-$username = $result->fetch();
-if (isset($_POST['liked'])) {
-    $postid = $_POST['postid'];
-    $result = $mysql->Execute("SELECT * FROM sadaf.post WHERE postId=$postid");
-    $row = $result->fetch();
-
-    $mysql->Execute("INSERT INTO sadaf.likes (username, userId, postId, status) VALUES ('".$username['username']."',".$userid.",".$postid." , '1')");
-    $result1 = $mysql->Execute("SELECT * FROM sadaf.likes WHERE postId=$postid");
-    $num = $result1->fetch();
-    echo count($num);
-    exit();
-}
-if (isset($_POST['unliked'])) {
-    $postid = $_POST['postid'];
-    $result = $mysql->Execute("SELECT * FROM sadaf.post WHERE postId=$postid");
-    $row = $result->fetch();
-
-    $mysql->Execute("DELETE FROM sadaf.likes WHERE postId=".$postid." AND userId=".$_SESSION['UserID']);
-    $result1 = $mysql->Execute("SELECT * FROM sadaf.likes WHERE postId=$postid");
-    $num = $result1->fetch();
-    echo count($num);
-    exit();
-}
-
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -41,98 +14,204 @@ if (isset($_POST['unliked'])) {
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" type="" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
     <link rel="stylesheet" href="profile.css">
-    <script src="../jquery/jquery-3.4.1.min.js.txt"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="right_script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="loginStyle/images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="loginStyle/css/util.css">
+    <link rel="stylesheet" type="text/css" href="loginStyle/css/style.css">
+    <!--===============================================================================================-->
 </head>
+
+<?php
+
+$mysql = pdodb::getInstance();
+$mysql->Prepare("select * from sadaf.profile where userId=?");
+$res = $mysql->ExecuteStatement(array($_SESSION['UserID']));
+
+if ($trec = $res->fetch()) {
+    $userId = $trec['userId'];
+    $name = $trec['name'];
+    $username = $trec['username'];
+    $profimage = $trec['profileimage'];
+    $bio = $trec["bio"];
+} else
+    $message = "This person is not valid.";
+
+if(isset($_REQUEST["change"]))
+{
+    $username = $_REQUEST["UserName"];
+    $password = $_REQUEST["UserPassword"];
+    $password_repeat = $_REQUEST["UserPasswordRepeat"];
+    $fname = $_REQUEST["fname"];
+
+
+    if (!preg_match('/^[a-zA-Z0-9]{4,}$/', $username)){
+        $message_array[1] = "Username must have 4 characters at least.";
+        $validation = false;
+    }
+
+    if (!preg_match('/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/', $password)){
+        $message_array[2] = "Password must have 8 characters at least that include uppercase, lowercase & numbers.";
+        $validation = false;
+    }
+
+    if ($password != $password_repeat){
+        $message_array[3] = "Password & repeat password aren't same.";
+        $validation = false;
+    }
+
+    $mysql->Prepare("Select username from sadaf.user where username = ? and userId != ?");
+    $res = $mysql->ExecuteStatement(array($username, $_SESSION['UserID']));
+
+    if($trec = $res->fetch())
+    {
+        if($trec['username'] == $username){
+            $message_array[5] = "This username is already registered.";
+            $validation = false;
+        }
+    }
+    if($validation){
+
+        $mysql = pdodb::getInstance();
+
+        $mysql->Prepare("UPDATE sadaf.profile SET
+						    (username, name, bio) values (?, ?, ?)");
+        $res = $mysql->ExecuteStatement(array($username, $fname, "hello i'm ".$fname));
+
+        $mysql->Prepare("Select userid from sadaf.profile
+						    where name=? and username=?");
+        $res = $mysql->ExecuteStatement(array($fname, $username));
+
+
+
+        if($trec = $res->fetch())
+        {
+            $password_hashed = md5($password);
+
+            $mysql->Prepare("Insert into sadaf.user (username, pass) values (?, ?)");
+            $res = $mysql->ExecuteStatement(array($username, $password));
+        }
+
+        $_SESSION["UserName"] = $username;
+        header("Location: main.php");
+        die();
+    }
+}
+
+
+?>
 
 <body style="background-color:rgb(230, 252, 252) ">
 <?php include("header-top.php")?>
 
-    <div class="w-100" style="height:70px;"></div>
-    <div class=" d-flex" style="direction:rtl;: left">
-        <?php include("right_side.php");?>
-        
+<div class="w-100" style="height:70px;"></div>
+<div class=" d-flex" style="direction:rtl;: left">
+    <?php include("right_side.php");?>
+</div>
+<div class="profile2">
+    <div class="profile2-banner">
+        <div class="text-center content-middle">
+            <b><?php echo $bio ?> </b>
+        </div>
     </div>
-    <div class="profile2">
-            <div class="profile2-banner">
-            </div>
-            <div class="profile2-picture">
-                <a href="" class="follow text-white">Follow <span class="entypo-plus"></span></a>
-                <a href=""><img src="https://i.pravatar.cc/300?img=7"></a>
-                <span>Daniel Jack</span>
-                <br>
-                <small>(Jack Daniel)</small>
+
+    <div class="profile2-picture">
+        <?php echo "<a href=''><img src= $profimage ></a>"?>
+
+        <?php echo "<span> $name </span>"?>
+        <br>
+        <?php echo "<small> ($username) </small>"?>
+        <br>
+    </div>
+
+    <div class="wrap-login100 shadow-bottom">
+        <form class="login100-form validate-form" method="post">
+            <div class="wrap-input100 validate-input m-b-26" data-validate="Name is required">
+                <span class="label-input100">Name</span>
+                <input class="input100" type="text" name="fname" id="fname" placeholder="Enter name" value= <?php
+                echo $fname;
+                ?>>
+                <span class="focus-input100"></span>
             </div>
 
-            <div class="profile2-content">
-                <div class="content-middle">
-                    <div class="content-md-left">
-                        <img src="https://i.pravatar.cc/300?img=7">
-                    </div>
-                    <div class="content-md-middle">
-                        <div class="post-title-name">
-                            <a href="post.php">Daniel Jack</a><br>
-                        </div>
-                        <div class="post-title-time">
-                            <a href="post.php">Saturday 13:52</a>
-                        </div>
-                        <div class="post-desc">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie eleifend orci non varius. Integer et mauris quis neque blandit sagittis.
-                            <br>
-                            <img src="./images/Computer.jpg" class="w-100">
-                        </div>
-                        <div class="mt-5 bg-white w-100 px-2 py-2 shadow-bottom post pl-4">
-                            <p class="d-inline" style="font-size: 14px;">like 3 comment 1</p>
-                            <div class="w-100">
-                                <div class=" kadr w-100 d-flex post-detail">
-                                    <p class="text-right detail like">
-                                        <a href=""><i class="fa fa-thumbs-up"></i> LIKE</a>
-                                    </p>
-                                    <p class="text-right detail">
-                                        <a href=""><i class="fa fa-comments"></i> COMMENT</a>
-                                    </p>
-                                    <p class="text-right detail">
-                                        <a href=""><i class="fa fa-share"></i> SHARE</a>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="bg-gray comment p-3 shadow-bottom shadow-left">
-                                <div class="d-flex">
-                                    <img src="./images/profile.png">
-                                    <input type="text " class="comment-holder ml-3 mr-1 col-11" placeholder="Write a Comment and press enter">
-                                    <i class="fa fa-send mt-2" style="cursor:pointer;font-size:20px"></i>
-                                    <!-- <img class="ml-2" src="./images/plus.png"> -->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="w-100">
-                            <div class="bg-gray comment mt-4 " >
-                                <div class="d-flex h-auto">
-                                    <img src="./images/profile.png" class="mr-1">
-                                    <div style="word-wrap: break-word;">blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa</div>
-                                </div>
-                            </div>
-                            <div class="bg-gray comment mt-4 ">
-                                <div class="d-flex h-auto">
-                                    <img src="./images/profile.png" class="mr-1">
-                                    <div style="word-wrap: break-word;">blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa</div>
-                                </div>
-                            </div>
-                            <div class="bg-gray comment mt-4 ">
-                                <div class="d-flex h-auto">
-                                    <img src="./images/profile.png" class="mr-1">
-                                    <div style="word-wrap: break-word;">blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa blaalalalalal ballalala blaaaa</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+                <span class="label-input100">Username</span>
+                <input class="input100" type="text" name="UserName" id="UserName" placeholder="Enter username" value=<?php echo $_SESSION["UserName"]; ?>>
+                <span class="focus-input100"></span>
             </div>
-        </div>
+
+            <div class="wrap-input100 validate-input m-b-26">
+                <span class="label-input100">Bio</span>
+                <input class="input100" type="text" name="Bio" id="Bio" placeholder="Enter bio" value=<?php echo $bio; ?>>
+                <span class="focus-input100"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+                <span class="label-input100">Password</span>
+                <input class="input100" type="password" name="UserPassword" id="UserPassword" placeholder="Enter password" value=<?php
+                echo $password;
+                ?>>
+                <span class="focus-input100"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input m-b-18" data-validate = "Repeat Password is required">
+                <span class="label-input100">Re-Password</span>
+                <input class="input100" type="password" name="UserPasswordRepeat" id="UserPasswordRepeat" placeholder="Enter re-password" value=<?php
+                echo $password_repeat;
+                ?>>
+                <span class="focus-input100"></span>
+            </div>
+
+            <div class="container-login100-form-btn">
+                <button name="change" type="submit" class="login100-form-btn" onclick=<?php
+                erase_val($message_array);
+                ?>>
+                    Register
+                </button>
+            </div>
+        </form>
+    </div>
+
+</div>
+
+<!--===============================================================================================-->
+<script src="loginStyle/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="loginStyle/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="loginStyle/vendor/bootstrap/js/popper.js"></script>
+<script src="loginStyle/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="loginStyle/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="loginStyle/vendor/daterangepicker/moment.min.js"></script>
+<script src="loginStyle/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="loginStyle/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="loginStyle/js/main.js"></script>
+
 </body>
 
 </html>
